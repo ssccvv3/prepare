ca7d407f9bfc45840be68827ae5a5e1c2a7f18fc
jun <ssccvv3wnstj@naver.com>
2022-12-16 19:34:23 +0900

    사칙연산 추가, 백준 맞춤

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ var a = parseInt(input[0])
 var b = parseInt(input[1])
 console.log(a*b)
 
+
+var fs= require('fs');
+var input= fs.readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(parseInt(a/b));
+console.log(a%b);
 // input 넣고 빼고 차이//
